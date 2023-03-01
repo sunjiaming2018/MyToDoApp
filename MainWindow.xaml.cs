cef141d83ff9dbead7ed583e2e983b5ed6c2ac1c
sunjiaming <1094652298@qq.com>
2023-03-02 00:42:26 +0800

    hello world

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MyToDoApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hello world!");
+        }
     }
 }
