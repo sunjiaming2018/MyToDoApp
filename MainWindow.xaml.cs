b6805f8587256794b0ed8d786c5468f54b20e32d
sunjiaming <1094652298@qq.com>
2023-04-03 02:21:09 +0800

    增加标题栏事件

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace MyToDoApp;
 
@@ -10,5 +11,38 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
+        btnMax.Click += (s, e) =>
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        };
+        btnClose.Click += (s, e) => { this.Close();};
+        ColorZone.MouseMove += (s, e) =>
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        };
+        ColorZone.MouseDoubleClick += (s, e) =>
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        };
     }
+
+
 }
