7e962c7678bf87c6daf5ffa0a7c50dcace4c7937
sunjiaming <1094652298@qq.com>
2023-04-13 03:04:18 +0800

    待办事项ui

--- ViewModels/ToDoViewModel.cs
+++ ViewModels/ToDoViewModel.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyToDoApp.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace MyToDoApp.ViewModels
 {
-    internal class ToDoViewModel
+    public class ToDoViewModel : BindableBase
     {
+        public ToDoViewModel()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDto>();
+            CreateToDoList();
+            AddCommand = new DelegateCommand(Add);
+        }
+
+        private bool isRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; }
+        }
+
+        private void Add()
+        {
+            IsRightDrawerOpen = true;
+        }
+        public DelegateCommand AddCommand { get; private set; }
+
+        private ObservableCollection<ToDoDto> toDoDtos;
+
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; RaisePropertyChanged();}
+        }
+
+        void CreateToDoList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                ToDoDtos.Add(new ToDoDto()
+                {
+                    Title = "标题" + i,
+                    Content = "测试数据..."
+                });
+            }
+        }
     }
 }
